d9349e1c366867258fbe41bd7034e46b26b283cb
ManiacUrgency <leonard.jin.0707@gmail.com>
2025-09-16 22:55:25 -0500

    Small bug fix Email config

--- dev_site/dev_site/dev_site/settings.py
+++ dev_site/dev_site/dev_site/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ['EMAIL_HOST']
-EMAIL_PORT = int(os.environ('EMAIL_PORT'))
+EMAIL_PORT = int(os.environ['EMAIL_PORT'])
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_SSL = os.environ['EMAIL_USE_SSL'] == 'True'
